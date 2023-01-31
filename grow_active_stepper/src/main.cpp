e541ea22471b280c0986d8510822d7085c0e2ba2
Tobias Sandmann <tobs.sandmann@gmail.com>
2023-01-31 15:19:09 +0100

    Fetch data from server and move stepper.

--- grow_active_stepper/src/main.cpp
+++ grow_active_stepper/src/main.cpp
@@ -1,14 +1,50 @@
 #include <Arduino.h>
 #include <AccelStepper.h>
+#include <Arduino_JSON.h>
+#include <HTTPClient.h>
 #include <Preferences.h>
-
-Preferences preferences;
+#include <WiFi.h>
 
 const int STEP = 19;
 const int ENABLE = 22;
 const int DIR = 23;
 
+const char* ssid = "Pixel 6";
+const char* password = "t6bzeq2knmyr6yc";
+String serverName = "http://192.168.10.119:8008";
+
+String serverReadings;
+int activeMinutes = 0;
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 60000;
+
+Preferences preferences;
 AccelStepper stepper(AccelStepper::DRIVER, STEP, DIR);
+
+String httpGETRequest(String serverName) {
+  WiFiClient client;
+  HTTPClient http;
+    
+  http.begin(client, serverName);
+  
+  int httpResponseCode = http.GET();
+  
+  String payload = "{}"; 
+  
+  if (httpResponseCode > 0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  http.end();
+
+  return payload;
+}
 
 void printPosition() {
   Serial.printf("Current position: %d\n", stepper.currentPosition());
@@ -20,10 +56,24 @@ void goToPosition(long pos) {
   printPosition();
   preferences.begin("stepper", false);
   preferences.putLong("position", stepper.currentPosition());
+  delay(3000);
+}
+
+void connectToWiFi() {
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("\n\nConnected to WiFi network with IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void setup() {
   Serial.begin(115200);
+
+  connectToWiFi();
 
   preferences.begin("stepper", true);
   long current_position = preferences.getLong("position", 0L);
@@ -38,13 +88,19 @@ void setup() {
   stepper.setAcceleration(10.0F);
   delay(1000);
   stepper.enableOutputs();
-  for (size_t i = 0; i < 5; ++i) {
-    if (stepper.currentPosition() < i * 10000) {
-      goToPosition(i * 10000);
-    }
-  }
-  goToPosition(0);
 }
 
 void loop() {
+  if ((millis() - lastTime) > timerDelay) {
+    if (WiFi.status() == WL_CONNECTED) {
+      serverReadings = httpGETRequest(serverName);
+      JSONVar myObject = JSON.parse(serverReadings);
+
+      JSONVar keys = myObject.keys();
+      activeMinutes = int(myObject[keys[0]]);
+      Serial.println(activeMinutes);
+      goToPosition(activeMinutes * 1000);
+    }
+    lastTime = millis();
+  }
 }
