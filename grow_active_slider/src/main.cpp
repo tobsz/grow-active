7cdc141ccd15ab883903b7e5fb1e3c814ccc1cde
Tobias Sandmann <tobs.sandmann@gmail.com>
2023-02-05 11:42:17 +0100

    Improve detection that the fader is in a stable position.

--- grow_active_slider/src/main.cpp
+++ grow_active_slider/src/main.cpp
@@ -3,29 +3,48 @@
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 
+enum direction_t {
+  UP,
+  DOWN,
+};
+
+struct valueAndDir {
+  int value;
+  direction_t dir;
+};
+
+direction_t dir = UP;
+int lastVal;
+
 const char *ssid = "thepromisedLAN";
 const char *password = "67192893961044349985";
-
 const char *serverName = "http://p14s:8000/activity";
 
-const int BUFLEN = 10;
-int buf[BUFLEN];
-int bufStart = 0;
-int bufEnd = 1;
+const size_t BUFLEN = 10;
+struct valueAndDir buffer[BUFLEN];
+size_t bufStart;
+size_t bufEnd;
 
-void putCB(int item) {
-  buf[bufStart++] = item;
-  bufStart %= BUFLEN;
+void resetCB() {
+  for (size_t i = 0; i < BUFLEN; ++i) {
+    buffer[i].value = -1;
+    buffer[i].dir = UP;
+  }
+  bufStart = 0;
+  bufEnd = 1;
 }
 
-int getCB() {
-  int item = buf[bufEnd++];
-  bufEnd %= BUFLEN;
-  return item;
+void putCB(int item, direction_t dir) {
+  buffer[bufStart].value = item;
+  buffer[bufStart].dir = dir;
+  ++bufStart %= BUFLEN;
 }
 
-
-
+valueAndDir getCB() {
+  valueAndDir valAndDir = buffer[bufEnd];
+  ++bufEnd %= BUFLEN;
+  return valAndDir;
+}
 
 void httpPost(int minutesVal) {
   if (WiFi.status() == WL_CONNECTED) {
@@ -54,19 +73,29 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   pinMode(A0, INPUT);
+  resetCB();
+  lastVal = -1;
 }
 
 void loop() {
-  int pot = analogRead(A0) / 34;
-  putCB(pot);
-  Serial.println(pot);
-  if (pot == getCB()) {
-    httpPost(pot);
-    int curPot = pot;
-    while (pot == curPot) {
-      pot = analogRead(A0) / 34;
-      delay(100);
-    }
+  int value = analogRead(A0) / 34;
+  if (value > lastVal && dir == DOWN) {
+    dir = UP;
+    resetCB();
+  } else if (value < lastVal && dir == UP) {
+    dir = DOWN;
+    resetCB();
   }
-  delay(100);
+  putCB(value, dir);
+  Serial.println(value);
+  valueAndDir vad = getCB();
+  if (value == vad.value && value == lastVal && dir == vad.dir) {
+    httpPost(value);
+    while (value == (analogRead(A0) / 34)) {
+      delay(50);
+    }
+  } else {
+    delay(150);
+  }
+  lastVal = value;
 }
