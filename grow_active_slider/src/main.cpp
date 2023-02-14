dd2977cac7aa7f6424ed302901c8255cc697fa93
Tobias Sandmann <tobs.sandmann@gmail.com>
2023-02-14 09:15:23 +0100

    Change ESPs to https.

--- grow_active_slider/src/main.cpp
+++ grow_active_slider/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
 #include <ESP8266HTTPClient.h>
-#include <WiFiClient.h>
 
 enum direction_t {
   DOWN,
@@ -21,8 +21,44 @@ int lastVal;
 // const char *url = "http://grow-active:8000/activity";
 const char *ssid = "Pixel 6";
 const char *password = "t6bzeq2knmyr6yc";
-const char *url = "http://werkverzeichnis.orijauch.de/activity";
+const char *url = "https://werkverzeichnis.orijauch.de/activity";
 // const char *url = "http://192.168.82.119:8000/activity";
+
+const char IRG_Root_X1 [] PROGMEM = R"CERT()
+  -----BEGIN CERTIFICATE-----
+  MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+  TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+  cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+  WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+  ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+  MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+  h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+  0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+  A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+  T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+  B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+  B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+  KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+  OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+  jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+  qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+  rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+  HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+  hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+  ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+  3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+  NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+  ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+  TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+  jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+  oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+  4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+  mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+  emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+  -----END CERTIFICATE-----
+)CERT";
+
+X509List cert(IRG_Root_X1);
 
 const size_t BUFLEN = 10;
 struct valueAndDir buffer[BUFLEN];
@@ -51,17 +87,27 @@ valueAndDir getCB() {
 }
 
 void httpPost(int minutesVal) {
-  if (WiFi.status() == WL_CONNECTED) {
-    WiFiClient client;
-    HTTPClient http;
+  WiFiClientSecure client;
 
-    http.begin(client, url);
-    http.addHeader("Content-Type", "application/json");
+  if ((WiFi.status() == WL_CONNECTED)) {
+    // client.setTrustAnchors(&cert);
+    client.setInsecure();
+    client.connect(url, 443);
+    HTTPClient https;
 
-    String buffer = "{\"minutes\":" + String(minutesVal) + "}";
-    int httpResponseCode = http.POST(buffer);
-    Serial.printf("HTTP response code: %d\n", httpResponseCode);
-    http.end();
+    if (https.begin(client, url)) {
+      https.addHeader("Content-Type", "application/json");
+      String buffer = "{\"minutes\":" + String(minutesVal) + "}";
+      int httpResponseCode = https.POST(buffer);
+      if (httpResponseCode < 0) {
+        Serial.printf(https.errorToString(httpResponseCode).c_str());
+      } else {
+        Serial.printf("HTTP response code: %d\n", httpResponseCode);
+      }
+    }
+    https.end();
+  } else {
+    Serial.printf("[HTTPS] Unable to connect.\n");
   }
 }
 
@@ -78,14 +124,29 @@ int readValue() {
 
 void setup() {
   Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  Serial.print("Connecting");
+  Serial.print("Connecting to WiFi ");
   while(WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.print("\nConnected to WiFi network with IP address:");
   Serial.println(WiFi.localIP());
+
+  configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+  Serial.println("Waiting for NTP time sync:");
+  time_t now = time(nullptr);
+  while (now < 8 * 3600 * 2) {
+    delay(500);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println();
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.print("Current time:");
+  Serial.print(asctime(&timeinfo));
 
   pinMode(A0, INPUT);
   resetCB();
